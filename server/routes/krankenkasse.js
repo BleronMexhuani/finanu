0e7c301a19cf3f7364e235aa655313be20de2ead
BleronMexhuani <58087682+BleronMexhuani@users.noreply.github.com>
2022-11-08 17:04:10 +0100

    Modal krankenkasse

--- server/routes/krankenkasse.js
+++ server/routes/krankenkasse.js
@@ -1,5 +1,4 @@
 var express = require("express");
-
 var krankenkasse = require('../controllers/krankenkasse.js');
 
 const router = express.Router();
@@ -8,7 +7,6 @@ router.get('/insurances/:plz/:ort/:commune', krankenkasse.getInsurance);
 router.get('/regions', krankenkasse.getRegions);
 router.get('/actualmodel/:id', krankenkasse.getActualModel);
 router.get('/compareInputs/:versicherer/:kanton/:region/:jahrgang/:accident/:tariftyp/:franchise/:model', krankenkasse.compareInputs);
+router.post('/sendMail', krankenkasse.sendMailKrakenkasse);
 
 module.exports = router
-
-
