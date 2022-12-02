88398eef25c425529b5302285671cfaf9f20e420
BleronMexhuani <58087682+BleronMexhuani@users.noreply.github.com>
2022-12-02 13:53:57 +0100

    Select Car at Automotorrad and on back changes,Bleroni hello team

--- server/routes/automotorrad.js
+++ server/routes/automotorrad.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get('/getMarke', automotorrad.getMarke);
 router.get('/getModell/:actualMarke',automotorrad.getModell);
+router.get('/getModellJahr/:actualMarke/:modell',automotorrad.getModellJahr);
 
 module.exports = router
